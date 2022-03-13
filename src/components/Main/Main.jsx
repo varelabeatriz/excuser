1ef04e6877ec5977458648aa5da66e2fd884b411
Beatriz Varela <beatrizsv19@gmail.com>
2022-03-13 20:56:30 -0300

    Adding filters to buttons

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,37 +4,39 @@ import './style.scss'
 
 export function Main() {
 
+    const [query, setQuery] = useState({
+        value: 'family'
+    });
     const [excuse, setExcuse] = useState();
 
-    const url = `https://excuser.herokuapp.com/v1/excuse`;
+    const url = `https://excuser.herokuapp.com/v1/excuse/${query}`;
 
-    const getData = async () => {
+    const getData = async (category) => {
+        setQuery(category); 
         const result = await Axios.get(url);
-        setExcuse(result)
+        setExcuse(result);
     }
 
     return (
         <div className='container'>
             <h2>Generate your Excuses</h2>
             <div className='button-container'>
-                <button onClick={() => getData()}>random</button>
-                <button>family</button>
-                <button>office</button>
-                <button>children</button>
-                <button>college</button>
-                <button>party</button>
+                <button onClick={() => getData('')}>random</button>
+                <button onClick={() => getData('family')}>family</button>
+                <button onClick={() => getData('office')}>office</button>
+                <button onClick={() => getData('children')}>children</button>
+                <button onClick={() => getData('college')}>college</button>
+                <button onClick={() => getData('party')}>party</button>
              </div>
             {excuse ? (
                 <div className='image-container'>
                     <img src="/images/conversation.svg" alt="conversation" />
-                    <p className='excuse'>{excuse.data[0].excuse}</p>
+                    <p className='excuse'>{(excuse.data[0].excuse)}</p> )
                 </div>
 
             ) : ( 
-                <span></span>
+                <span>hi</span>
              )}
-
-        
 
         </div>
     )
