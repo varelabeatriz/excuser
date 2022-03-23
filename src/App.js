46e6cd1bf2425eefefd22df5697d5201c2cfef78
Beatriz Varela <beatrizsv19@gmail.com>
2022-03-23 09:04:37 -0300

    Adding about page and using router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import { Main } from './components/Main/Main';
 import { Header } from './components/Header/Header';
+import { About } from './components/About/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Main></Main>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
